b386276f2a9d1d045c4d361953a1f4e543ced1fa
unknown <Mehul-HP@.(none)>
2010-11-29 22:31:40 -0500

    Added code to handle same word in multiple positions in a file.

--- Searcher/SearcherLib/Word.cs
+++ Searcher/SearcherLib/Word.cs
@@ -7,10 +7,23 @@ namespace SearcherLib
 {
     class Word
     {
-        Dictionary<DiskFile, int> WordLocationMap = new Dictionary<DiskFile, int>();
+        Dictionary<DiskFile, List<int>> WordLocationMap = new Dictionary<DiskFile, List<int>>();
         
         public string Text { get; set; }
 
+        public void Add(DiskFile df, int position)
+        {
+            if(!this.WordLocationMap.ContainsKey(df))
+            {
+                List<int> tmpPosList = new List<int>();
+                tmpPosList.Add(position);
+                this.WordLocationMap.Add(df, tmpPosList);
+            }
+            else
+            {
+                this.WordLocationMap[df].Add(position);
+            }
+        }
 
     }
 }
